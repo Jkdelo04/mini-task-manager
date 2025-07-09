c7ed39642014465f7d121027cc7e54b1c09ad932
Julia DeLorenzo <julia.k.delo@gmail.com>
2025-07-08 20:44:26 -0600

    setup flask login

--- app.py
+++ app.py
@@ -9,7 +9,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SECRET_KEY'] = 'your_secret_key'
 db.init_app(app)
 
+# Create Tables
+with app.app_context():
+    db.create_all()
+
+# Flask login
+login_manager = LoginManager()
+# redirects here if @login_required fails
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
+@login_required
 
 
 
