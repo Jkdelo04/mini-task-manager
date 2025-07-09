d370107503ffc6c06d617f238e10265eebae582c
Julia DeLorenzo <julia.k.delo@gmail.com>
2025-07-08 21:31:46 -0600

    Create database

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 
 from flask import Flask, render_template, redirect, url_for, request
-from models import db, User, Task
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey' 
 
 @app.route('/')
 
