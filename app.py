03a0abd375ac86da96db5cf5b7686e6fde1b0fe5
Julia DeLorenzo <julia.k.delo@gmail.com>
2025-07-08 20:35:43 -0600

    Create data base
    sets up the user database

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from models import db, User, Task
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db.init_app(app)
+
 @app.route('/')
+
+
 
 def home():
     return "Hello world"
@@ -10,4 +19,3 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
